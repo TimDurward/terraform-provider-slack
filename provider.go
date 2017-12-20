031ad15d0023c8dc40dd151b92218794bb4a2618
Tim Durward <timothy.a.durward@gmail.com>
2017-12-19 23:53:14 -0800

    build out slack provider

---
+++ provider.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"api_token": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("API_TOKEN", nil),
+				Description: "Slack Authentication Token for api.slack.com",
+			},
+		},
+		ResourcesMap:  map[string]*schema.Resource{},
+		ConfigureFunc: configureProvider,
+	}
+}
+
+func configureProvider(d *schema.ResourceData) (interface{}, error) {
+	/*
+		Config: APIToken
+		A string value used to authenticate new slack clients
+		i.e api := slack.New(m.(*Config).APIKey)
+	*/
+	config := &Config{
+		APIToken: d.Get("api_token").(string),
+	}
+	return config, nil
+}
---
+++ provider_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider().(*schema.Provider)
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvider_impl(t *testing.T) {
+	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+}
